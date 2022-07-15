c9406725522fb97817105a6c0bd4e33041674531
DennyAzevedo <denny_af@yahoo.com.br>
2022-07-14 22:17:45 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-	return <h1>Olá Mundo!!</h1>;
-}
+import NotificationButton from "./components/NotificationButton";
 
-export default App;
+export default function App() {
+	return (
+		<>
+			<h1>Olá Mundo!!</h1>
+			<NotificationButton />
+			<NotificationButton />
+		</>
+	);
+}
